80b76c035fedcccf874f8c0c349445411f9d6957
Aaron Mayhew <standingbytocopy@gmail.com>
2021-07-07 12:20:08 -0500

    added css modules for the user/game/class component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import classes from './AddUser.module.css';
+import Card from '../UI/Card';
+
+
+
 const AddUser = (props) => {
 
   const addUserHandler = (event) => {
@@ -7,13 +12,17 @@ const AddUser = (props) => {
   };
 
   return (
-  <form onSubmit={addUserHandler}>
-    <label htmlFor="username">Username</label>
-    <input id="username" type="text" />
-    <label htmlFor="age">Age (Years)</label>
-    <input id="age" type="number" />
-    <button type="submit">Add User</button>
-  </form>
+  <Card className={classes.input}>
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+      <label htmlFor="email">Email</label>
+      <input id="email" type="text" />
+      <button type="submit">Email address</button>
+    </form>
+  </Card>
   );
 };
 
