c97fa62b28d8f7ed6b0cd49824edd32f64557630
Aaron Mayhew <standingbytocopy@gmail.com>
2021-07-21 11:54:14 -0500

    dynamic user list done, needs styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 
 
@@ -11,18 +12,27 @@ const AddUser = (props) => {
   const [enteredUserName, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
   const [enteredEmail, setEnteredEmail] = useState('');
+  const [error, setError] = useState();
 
 
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0 || enteredEmail.trim().length === 0) {
+        setError({
+          title: 'Invalid input',
+          message: 'Please enter a valid name and age (non-empty values).'
+        });
       return;
     }
     if (+enteredAge < 1) {
-
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a validage.'
+      });
+      return;
     }
-    console.log(enteredUserName, enteredAge, enteredEmail);
+    props.onAddUser(enteredUserName, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
     setEnteredEmail('');
@@ -40,18 +50,25 @@ const AddUser = (props) => {
     setEnteredEmail(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" value={enteredUserName} onChange={usernameChangeHandler} />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={enteredEmail} onChange={emailChangeHandler} />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && (<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />)}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" value={enteredUserName} onChange={usernameChangeHandler} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" value={enteredEmail} onChange={emailChangeHandler} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card> 
+    </div>
   );
 };
 
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -7,7 +7,11 @@ const UsersList = (props) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map(user => <li>{user.name} ({user.age} years old)</li>)}
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
       </ul>
     </Card>
   );
